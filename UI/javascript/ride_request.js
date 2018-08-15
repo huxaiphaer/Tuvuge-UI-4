a19b35b329ed409d71efc62646a8094a20d6b0ca
Idris Lutaaya <idrislutaaya@Idriss-MacBook-Pro.local>
2018-08-15 13:04:37 +0300

    adjust ride request

--- UI/javascript/ride_request.js
+++ UI/javascript/ride_request.js
@@ -1,10 +1,8 @@
 function getRideRequests() {
-          
-    // Nested another fetch to get a specific ID.
-    fetch('https://tuvuge-app.herokuapp.com/api/v1/rides/' + 1,
+    fetch('https://tuvuge-app.herokuapp.com/api/v1/rides/1',
         {
             method: 'GET',
-            mode: "no-cors",
+            cache: 'no-cache',
             headers: {
                 'Accept': 'application/json',
                 'Content-type': 'application/json',
@@ -12,39 +10,40 @@ function getRideRequests() {
             }
         })
         .then((res) => {
-            console.log('my status',res.status);
             if (res.status == '401') {
-                console.log('my status',res.status);
+                console.log('my status', res.status);
                 alert("Your session has expired. Try logging in again");
                 window.location.href = "Driver_accounts.html";
             }
+            //console.log("check data : ", res.json());
             return res.json();
         })
 
         .then((res) => {
-            console.log('my new status',res.ride_offer);
+            console.log('my new status', res.ride_offer);
             var jdata = "" + res.ride_offer;
             var str = "" + jdata.replace(/'/g, "\"");
             obj_two = JSON.parse(str);
+            console.log("check two : ", obj_two);
 
 
             let output = `
-    <div class="container">
-    <h4>
-        <b>${obj.id}</b>
-    </h4>
-    <p>Passenger Name  : ${obj.passengername}</p>
-    <p>Price: ~ <strong> ${obj_two.price} </strong> ugx</i> </p>
-    <p>Ride Details:  <strong> ${obj_two.details} </strong></i> </p>
-    <hr/>
-    <button>
-        <i class="fa fa-remove"> Cancel Request</i>
-    </button>
-    <button>
-        <i class="fa fa-check-square-o"> Accept Request</i>
-    </button>
-    </div>
-    `;
+                <div class="container">
+                    <h4>
+                        <b>${obj_two.id}</b>
+                    </h4>
+                    <p>Passenger Name : ${obj_two.driver}</p>
+                    <p>Price: ~ <strong> ${obj_two.price} </strong> ugx</i> </p>
+                    <p>Ride Details: <strong> ${obj_two.details} </strong></i> </p>
+                        <hr/>
+                    <button>
+                        <i class="fa fa-remove"> Cancel Request</i>
+                    </button>
+                    <button>
+                        <i class="fa fa-check-square-o"> Accept Request</i>
+                    </button>
+                </div>
+                `;
             document.getElementById('requests_card').innerHTML = output;
         })
 
